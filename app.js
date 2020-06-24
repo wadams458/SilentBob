96c6899845182a26380291c5434dfb72437e94d7
wadams <wwadams94@gmail.com>
2020-06-24 15:10:16 -0700

    timer works p.1

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ console.log("Hi im Silent Bob");
 // Save Button in Variable
 // Add click event listener to button
 
+//--------App State
+let time = 3;
+
 //---------- Cached DOM Elements
 
 const startButton = document.getElementById("startGame");
@@ -17,5 +20,20 @@ startButton.addEventListener("click", handleStartGame);
 //----------- Function Declarations
 
 function handleStartGame() {
-  //console.log("Start Game clicked...!");
+  console.log("Start Game clicked...!");
+
+  // Start Timer
+  startTimer();
+}
+
+function startTimer() {
+  const Timer = setInterval(function () {
+    if (time > 0) {
+      time--;
+      console.log(time);
+    } else {
+      console.log("Time is up");
+      clearInterval(Timer);
+    }
+  }, 1000);
 }
