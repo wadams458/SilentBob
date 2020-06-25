794146ff9ccceb00fb8a46c17e037bf5815fe1eb
wadams <wwadams94@gmail.com>
2020-06-24 22:04:09 -0700

    increment function

--- app.js
+++ app.js
@@ -12,10 +12,14 @@ const hourglass = document.querySelector(".hourglass");
 const bone = document.querySelector(".bone");
 //const bone = 10;
 let time = 3;
+let score = 0;
 
 //---------- Cached DOM Elements
 
 const startButton = document.getElementById("startGame");
+// const boneContainer = document.querySelector(.bone);
+// const hourGlassContainer = document.querySelector(.hourGlass);
+// const bedContainer = document.querySelector(.bed);
 
 //------------Event Listeners
 
@@ -62,16 +66,72 @@ function updateTime() {
   //timer.innerText = time;
 }
 
+//Event  Delegation
+
 function handleBoneClick(event) {
-  console.log("bone Clicked!");
+  console.log(event.target.classList.contains("bone"));
+  {
+    //const bone = event.target.style.button;
+
+    //if (event.target.classList.contains("bone")) {
+    //boneContainer.removeChild(event.target);
+    checkScore(play);
+  }
 }
 
 function handleBedClick(event) {
   console.log("bed Clicked!");
+  //f (event.target.classList.contains("bed")) {
 }
 
 function handleHourglassClick(event) {
   console.log("hourGlass Clicked!");
+  //if (event.target.classList.contains("hourGlass")) {
+}
+
+function checkScore(play) {
+  //if play = boredom; score + 1
+  //else score = score - 1
+
+  if (play === "boredom") {
+    //score = score -1
+    score++;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  } else {
+    //score = score -1
+    score--;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  }
+}
+
+function checkScore(Eat) {
+  //if play = boredom; score + 1
+  //else score = score - 1
+
+  if (Eat === "sleepiness") {
+    //score = score -1
+    score++;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  } else {
+    //score = score -1
+    score--;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  }
+}
+
+function checkScore(Nap) {
+  //if play = boredom; score + 1
+  //else score = score - 1
+
+  if ("sleepiness" === "boredom") {
+    //score = score -1
+    score++;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  } else {
+    //score = score -1
+    score--;
+    //document.querySelector.('h1').innerText = `Day: $(score)`;
+  }
 }
 
 // Add a class to the new div
