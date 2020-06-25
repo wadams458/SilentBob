b5520719ea21664253870fe7fc9e82541b180eab
wadams <wwadams94@gmail.com>
2020-06-24 21:14:25 -0700

    Icons Click

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ console.log("Hi im Silent Bob");
 // Add click event listener to button
 
 //--------App State
+const bed = document.querySelector(".bed");
+const hourglass = document.querySelector(".hourglass");
+const bone = document.querySelector(".bone");
+//const bone = 10;
 let time = 3;
 
 //---------- Cached DOM Elements
@@ -16,6 +20,14 @@ const startButton = document.getElementById("startGame");
 //------------Event Listeners
 
 startButton.addEventListener("click", handleStartGame);
+
+//Event Delegation
+//Event listeners are registered when the DOM Loads
+// To fix this problem use event delegation (assigning listener to parent)
+
+bone.addEventListener("click", handleBoneClick);
+bed.addEventListener("click", handleBedClick);
+hourglass.addEventListener("click", handleHourglassClick);
 
 //----------- Function Declarations
 
@@ -49,6 +61,26 @@ function updateTime() {
 
   //timer.innerText = time;
 }
+
+function handleBoneClick(event) {
+  console.log("bone Clicked!");
+}
+
+function handleBedClick(event) {
+  console.log("bed Clicked!");
+}
+
+function handleHourglassClick(event) {
+  console.log("hourGlass Clicked!");
+}
+
+// Add a class to the new div
+//bone. className = 'bone';
+//Add classes to class list
+
+bone.classList.add("bone");
+hourglass.classList.add("hourglass");
+bed.classList.add("bed");
 
 // Eat Timer
 
